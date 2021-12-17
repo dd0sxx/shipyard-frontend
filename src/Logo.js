dd28f2952f036ce79daab371650e9e072ba007e7
dd0sxx <contacttheotelonis@gmail.com>
2021-12-16 21:39:33 -0500

    started design

---
+++ src/Logo.js
@@ -0,0 +1,15 @@
+import { div } from 'prelude-ls';
+import React from 'react'
+
+function Logo () {
+
+    
+
+  return (
+    <div className="Logo" style={{fontSize: 72, fontFamily: 'helvetica', textAlign: 'center', padding: '10vh', color: 'white'}}>
+      Ship Yard Studios
+    </div>
+  );
+}
+
+export default Logo;
