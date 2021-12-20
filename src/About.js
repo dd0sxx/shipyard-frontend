e34e662c122d2cdc1d335bdcaf776913742e138b
dd0sxx <contacttheotelonis@gmail.com>
2021-12-20 15:47:19 -0500

    fixed copy in team section

--- src/About.js
+++ src/About.js
@@ -7,7 +7,7 @@ function About () {
     return(
         <div className="about">
             <div className="big-text assistant">Shipyard Studios</div>
-            <div className="small-text abel">We work with creahrefrs of all mediums and enable them href take control of their intellectual property and provenance by giving them true ownership of their creations. Whether you are a musician, visual artist, film-maker, or brand we give you the hrefols + strategy you need href begin creating in web3.</div>
+            <div className="small-text abel">We work with creators of all mediums and enable them to take control of their intellectual property by giving them true ownership of their creations. Whether you are a musician, visual artist, film-maker, or brand we give you the tools + strategy you need to begin creating in web3.</div>
             <div className="medium-text assistant">The Team:</div>
             <div className="flex" style={{width: '60vw', margin: 'auto', padding: '5vh'}}>
 
