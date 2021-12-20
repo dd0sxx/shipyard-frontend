e13e14bfc3006f61c8e125bf955e5add6d5cf936
dd0sxx <contacttheotelonis@gmail.com>
2021-12-19 21:05:14 -0500

    finished collabs

--- src/Colab.js
+++ src/Colab.js
@@ -12,12 +12,13 @@ function Colab () {
     
     return (
         <div className="colab">
-            <div className="small-text">Projects we have shipped: </div>
+            <div className="small-text abel">Projects we have shipped: </div>
             <div className="flex" style={{width: '60vw', margin: 'auto', paddingTop: '5vh'}}>
             {collaborations.map(item => (
-                <div>
+                <div style={{alignItems: 'center', display: 'flex', flexDirection: 'column'}}>
                     <div className='background-center' style={{backgroundImage: `url("${item.img}")`, height: 150, width: 150, borderRadius: '50%'}}/>
-                    <div className="small-text" style={{textAlign: 'center', marginTop: 15, width: 150, padding: 0}}>{item.name}</div>
+                    <div className="small-text assistant" style={{textAlign: 'center',color: 'rgb(225, 218, 207)', marginTop: 15, width: 200, padding: 0}}>{item.name}</div>
+                    <div className="small-text abel" style={{textAlign: 'center',fontSize: 16, marginTop: 10, width: 150, padding: 0}}>by {item.creator}</div>
                 </div>
             ))}
             </div>
--- src/Logo.js
+++ src/Logo.js
@@ -7,7 +7,7 @@ function Logo () {
   return (
     <div className="Logo" style={{
         fontSize: 36, 
-        fontFamily: 'helvetica', 
+        fontFamily: 'quicksand', 
         textAlign: 'left', 
         color: 'black',
         }}>Ship Yard Studios</div>
