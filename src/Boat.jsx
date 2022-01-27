6de8a7df259f1327183fae47660da4d269e2d64d
dd0sxx <contacttheotelonis@gmail.com>
2022-01-26 20:19:34 -0700

    fixed boat position y on resize

--- src/Boat.jsx
+++ src/Boat.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import BoatGif from './assets/boat.gif';
 
 function Boat() {
+
+ const [boatYPosition, setBoatYPosition] = useState(document.body.clientHeight * 0.26 - 198)
+
+ useEffect(() => {
+  window.addEventListener('resize', () => setBoatYPosition(document.body.clientHeight * 0.26 - 198))
+ }, [])
+
   return (
     <div
       className="Boat background-center"
       style={{
         backgroundImage: `url("${BoatGif}")`,
         position: 'absolute',
-        top: document.body.clientHeight * 0.26 - 198
+        top: boatYPosition
       }}
      />
   );
